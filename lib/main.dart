8ed9817c6fa5c9f39f0717c72629986710ca46d7
RIOKOWI <achyarrio2004@gmail.com>
2025-10-24 11:20:05 +0700

    create material app

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp();
   }
 }
+
+// RunApp
+// MaterialApp
+// Scafold
